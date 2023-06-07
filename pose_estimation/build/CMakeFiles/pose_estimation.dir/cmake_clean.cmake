61d4511457e07bba1ece4581dd01f31f9f4d053c
Júlia <79165532+jumanascimento@users.noreply.github.com>
2023-06-07 13:54:36 -0300

    Pacote de detcção e calculo de distância 2.0

---
+++ pose_estimation/build/CMakeFiles/pose_estimation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pose_estimation.dir/src/main.o"
+  "pose_estimation"
+  "pose_estimation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose_estimation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
